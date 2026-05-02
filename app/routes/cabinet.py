e8896aaa4667df93c121ba256ea93637382d81f4
NaoTomori0 <kyongfam0@gmail.com>
2026-05-02 23:13:48 +0300

    Update cabinet.py

--- app/routes/cabinet.py
+++ app/routes/cabinet.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from app.models import Submission
+from app import cache
 
 bp = Blueprint("cabinet", __name__, url_prefix="/cabinet")
 
 
 @bp.route("/")
 @login_required
+@cache.cached(timeout=30, key_prefix=lambda: f"cabinet_{current_user.id}")
 def index():
     submissions = (
         Submission.query.filter_by(user_id=current_user.id)
